7fb8611110b60b7dd4d52b6e5a19d54bfd7fadf6
Evgheni Scobici <evghenii.scobici@endava.com>
2023-05-12 08:30:52 +0300

    next commit

--- src/modem_service.cpp
+++ src/modem_service.cpp
@@ -19,21 +19,59 @@ void ModemService::update(State* state) {
         modemState.nextCmd = MODEM_CMD_UNKNOWN;
         if (modemState.currentCmd == MODEM_CMD_CHECK_MODEM_AND_POWER_ON_IF_NEED) {
             sim800c->sendCmdAndSetExpectedPatternAndTimeout("AT\r\n", "OK'\r\n", 100);
+            modemState.previousState = modemState.currentState;
+            modemState.currentState = MODEM_STATE_RUNNING;
         }
     }
 
     SIM800CState sim800cState = sim800c->update();
+
+    if (modemState.currentState == MODEM_STATE_RUNNING) {
+        if (sim800cState.currentState == SIM800C_STATE_PATTERN_MATCHED) {
+            modemState.previousState = modemState.currentState;
+            modemState.currentState = MODEM_STATE_SUCCESS;
+        }
+
+        if (sim800cState.currentState == SIM800C_STATE_ERROR) {
+
+            if (numberOfRetries == 0) {
+                state->currentState = STATE_MODEM_STATE_ERROR;
+            } else {
+                numberOfRetries--;
+                modemState.nextCmd = modemState.currentCmd;
+                modemState.currentCmd = modemState.previousCmd;
+                modemState.previousCmd = MODEM_CMD_UNKNOWN;
+            }
+
+
+
+            modemState.previousState = modemState.currentState;
+            modemState.currentState = MODEM_STATE_ERROR;
+        }
+    }
 
 
     if (modemState.currentState != modemState.previousState) {
         modemState.previousState = modemState.currentState;
         if (modemState.currentState == MODEM_STATE_SUCCESS) {
             state->currentState = STATE_MODEM_POWERED_ON;
-        }
+        } 
+
+        if (modemState.currentState == MODEM_STATE_ERROR) {
+            if (numberOfRetries == 0) {
+                state->currentState = STATE_MODEM_STATE_ERROR;
+            } else {
+                numberOfRetries--;
+                modemState.nextCmd = modemState.currentCmd;
+                modemState.currentCmd = modemState.previousCmd;
+                modemState.previousCmd = MODEM_CMD_UNKNOWN;
+            }
+        } 
     }
 
 }
 
 void ModemService::checkModemAndPowerOnIfNeed() {
     modemState.nextCmd = MODEM_CMD_CHECK_MODEM_AND_POWER_ON_IF_NEED;
+    numberOfRetries = 2;
 }
